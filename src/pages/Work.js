c8b2dc26c9991122a6b6617149807057a43e985a
wszdexdrf <piyushkumar2k02@kgpian.iitkgp.ac.in>
2023-12-06 00:05:49 +0530

    Fix h1 color

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Work() {
     return (
         <div style={{ padding: '20px' }}>
-            <h1 style={{ color: 'orange', WebkitTextStroke: '1px white' }}>Manage Torrents</h1>
+            <h1 style={{ color: '#E95420', WebkitTextStroke: '1px white' }}>Manage Torrents</h1>
             <iframe src="http://127.0.0.1:9091/transmission/web/" title="Torrent Manager" width="100%" height="500px" />
         </div>
     );
